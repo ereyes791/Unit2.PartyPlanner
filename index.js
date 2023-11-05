9cfba63140886c33ee8db46ee8219ac8e924976c
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2023-11-05 14:48:05 -0600

    Get Events and Show it
    Get the last 10 elements and show it in the doc

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const eventAPI = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A"
-
+const events = [];
 async function getEvents() {
     try{
         const response = await fetch(eventAPI+"/events");
         const data = await response.json();
-        console.log(data);
+        for (let index = data.data.length -10; index < data.data.length; index++) {
+            events.push(data.data[index]);
+        }
+        events.forEach(element => {
+            createEventsElements(element);
+        });
+        console.log(events);
     }catch(err){
         console.log(err);
     }
 };
+function render(){
+    createEventsElements();
+}
+function createEventsElements(event){
+    // Create a new <article> element
+    const articleElement = document.createElement("article");
+    // You can set attributes and content for the <article> if needed
+    articleElement.id = event.id;
+    
+    // Create an <h2> element
+    const h2Element = document.createElement("h2");
+    h2Element.textContent = `Event Name: ${event.name}`; 
+    articleElement.appendChild(h2Element);
+    const pDate = document.createElement("p");
+    pDate.textContent = `Date :  ${new Intl.DateTimeFormat('en-US',
+        { year: 'numeric', month: 'short', day: 'numeric' })
+        .format(new Date(event.date))}`;
+    articleElement.appendChild(pDate);
+    const pLocation = document.createElement("p");
+    pLocation.textContent = `Location :  ${event.location}`;
+    articleElement.appendChild(pLocation);
+    const pDescription = document.createElement("p");
+    pDescription.textContent = `Description :  ${event.description}`;
+    articleElement.appendChild(pDescription);
+    document.body.appendChild(articleElement);
+}
 getEvents();
